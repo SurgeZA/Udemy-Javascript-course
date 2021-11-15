c699aa00cf877a72fd014738411f9f4d43430563
SurgeZA <Scottlogandennis@gmail.com>
2021-11-15 14:25:50 +0200

    first push

---
+++ challenges.js
@@ -0,0 +1,19 @@
+// Challenge one
+// const mark_height = 1.69;
+// const john_height = 1.95;
+// const mark_weight = 78;
+// const john_weight = 92;
+
+// let BMI = mark_weight / mark_height
+// let BMI = mark_Mass
+// let mark_Bmi = mark_Mass / mark_height
+
+// console.log(mark_Bmi)
+
+// let BMI = john_weight / john_height
+// let BMI = john_Mass
+// let john_Bmi = _Mass / mark_height
+
+// console.log(mark_Bmi)
+
+// Challenge 2
---
+++ udemy.js
@@ -0,0 +1,20 @@
+// Strings and literals
+// const firstName = "Jonas";
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2021;
+
+// const jonasNew = `I'm ${firstName} a ${year - birthYear} ${job}!`;
+
+// console.log(jonasNew);
+
+// const age = 15;
+// const isOldEnough = age >= 18;
+
+// if (age >= 18) {
+//     console.log('Sarah can start drivers license')
+// } else {
+
+//     const yearsLeft = 18 - age
+//     console.log(`Sarah needs to wait ${yearsLeft} years`)
+// }
